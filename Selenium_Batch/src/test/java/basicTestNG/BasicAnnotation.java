d0bd4438cabd917fa349e33c780a943dea2e6d3a
vishn <vishn@vishnu>
2024-07-29 10:28:53 +0530

    1 commit

---
+++ Selenium_Batch/src/test/java/basicTestNG/BasicAnnotation.java
@@ -0,0 +1,44 @@
+package basicTestNG;
+//Selenium not have report generation, Annotations can be used here, Different methods can be run
+
+//Unit framework-
+import org.testng.annotations.Test;
+//Run according to alphabetical order
+public class BasicAnnotation {
+	@Test
+	  public void testcase10() 
+	  {
+		System.out.println("testcase10"); 
+	  }
+  @Test
+  public void testcase01() 
+  {
+	System.out.println("testcase01"); 
+  }
+  
+  @Test
+  public void testcase02() 
+  {
+	System.out.println("testcase02"); 
+  }
+  @Test
+  public void testcase03() 
+  {
+	System.out.println("testcase03"); 
+  }
+  @Test
+  public void testcase04() 
+  {
+	System.out.println("testcase04"); 
+  }
+  @Test
+  public void testcase05() 
+  {
+	System.out.println("testcase05"); 
+  }
+  @Test
+  public void testcase06() 
+  {
+	System.out.println("testcase06"); 
+  }
+}
